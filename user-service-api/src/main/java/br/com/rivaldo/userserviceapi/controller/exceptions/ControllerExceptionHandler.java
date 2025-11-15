d4a0316b01a63c8f93e2c63d0e6ead1d2e8608bd
Rivaldo Souza <rivaldo.lsouza@outlook.com>
2025-11-15 15:40:03 -0300

    feat: add exception handler for object not found

---
+++ user-service-api/src/main/java/br/com/rivaldo/userserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.rivaldo.userserviceapi.controller.exceptions;
+
+import br.com.rivaldo.models.exceptions.ResourceNotFoundException;
+import br.com.rivaldo.models.exceptions.StandardError;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    ResponseEntity<StandardError> handleNotFoundException(final ResourceNotFoundException ex, final HttpServletRequest request) {
+        return ResponseEntity.status(NOT_FOUND).body(
+                StandardError.builder()
+                        .timeStamp(now())
+                        .status(NOT_FOUND.value())
+                        .error(NOT_FOUND.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .path(request.getRequestURI())
+                        .build()
+        );
+    }
+}
--- user-service-api/src/main/java/br/com/rivaldo/userserviceapi/service/UserService.java
+++ user-service-api/src/main/java/br/com/rivaldo/userserviceapi/service/UserService.java
@@ -1,5 +1,6 @@
 package br.com.rivaldo.userserviceapi.service;
 
+import br.com.rivaldo.models.exceptions.ResourceNotFoundException;
 import br.com.rivaldo.models.responses.UserResponse;
 import br.com.rivaldo.userserviceapi.mapper.UserMapper;
 import br.com.rivaldo.userserviceapi.repository.UserRepository;
@@ -14,7 +15,12 @@ public class UserService {
     private final UserMapper userMapper;
 
     public UserResponse findById(final String id) {
-        return userMapper.fromEntity(userRepository.findById(id).orElse(null));
+        return userMapper.fromEntity(
+                userRepository.findById(id)
+                        .orElseThrow(() -> new ResourceNotFoundException(
+                                "Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName()
+                        ))
+        );
     }
 
 }
